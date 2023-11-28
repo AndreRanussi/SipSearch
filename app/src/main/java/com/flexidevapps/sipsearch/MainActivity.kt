aa6f773718496cef8719637619c0da641eec0529
AndreRanussi <ranussi.a@gmail.com>
2023-11-28 15:04:01 +0000

    working in setting up the UI components for the project

--- app/src/main/java/com/flexidevapps/sipsearch/MainActivity.kt
+++ app/src/main/java/com/flexidevapps/sipsearch/MainActivity.kt
@@ -3,12 +3,17 @@ package com.flexidevapps.sipsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.flexidevapps.sipsearch.ui.theme.SipSearchTheme
 
@@ -16,13 +21,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SipSearchTheme {
+            window.statusBarColor = getColor(R.color.black)
+            window.navigationBarColor = getColor(R.color.black)
+               SipSearchTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SipSearch()
                 }
             }
         }
@@ -30,17 +37,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SipSearch(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SipSearchTheme {
-        Greeting("Android")
-    }
+    Box(
+        modifier = with(Modifier){
+            fillMaxSize()
+                .paint(
+                    painterResource(id = R.drawable.main_loading_image),
+                    contentScale = ContentScale.FillBounds
+
+                )
+        }
+    )
+
 }
--- app/src/main/java/com/flexidevapps/sipsearch/ui/theme/Theme.kt
+++ app/src/main/java/com/flexidevapps/sipsearch/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.flexidevapps.sipsearch.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -24,7 +25,8 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onSurface = black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -56,9 +58,6 @@ fun SipSearchTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
