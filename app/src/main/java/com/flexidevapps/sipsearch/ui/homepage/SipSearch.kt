6ddfc82dcc2a5e9a088c93f38df5c35ee845edf4
AndreRanussi <ranussi.a@gmail.com>
2023-11-29 07:33:05 +0000

    chore: created folder structure

--- app/src/main/java/com/flexidevapps/sipsearch/ui/homepage/SipSearch.kt
+++ app/src/main/java/com/flexidevapps/sipsearch/ui/homepage/SipSearch.kt
@@ -1,23 +1,21 @@
-package com.flexidevapps.sipsearch
+package com.flexidevapps.sipsearch.ui.homepage
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.flexidevapps.sipsearch.R
 import com.flexidevapps.sipsearch.ui.theme.SipSearchTheme
 
-class MainActivity : ComponentActivity() {
+class SipSearch : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
